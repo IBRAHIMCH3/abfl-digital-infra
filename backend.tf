d919df1999e1243773ae16e1ee4eb0e4369e7a65
IBRAHIM <59605442+IBRAHIMCH3@users.noreply.github.com>
2021-07-04 12:46:24 +0530

    backend.tf
    test

---
+++ backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    # S3 Bucket name
+    
+    bucket          = "abflibrahim"
+    #key            = "global/ec2/terraform.tfstate"
+    region          = "ap-southeast-1"
+    #role_arn       = "arn:aws:iam::123456789123:role/AdminRole"
+    
+    
+    #DynamoDB table name!
+    
+    #dynamodb_table = "ent-tf-statelock"
+    #encrypt        = true
+  }
+}
