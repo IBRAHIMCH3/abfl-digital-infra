e71836f0f68bf0578eef255d9910147450d2ac7e
IBRAHIM <59605442+IBRAHIMCH3@users.noreply.github.com>
2021-07-06 19:50:55 +0530

    Update ami.tf

--- ami.tf
+++ ami.tf
@@ -1,10 +1,4 @@
-provider "aws" {
-  region  = var.aws_region
-    assume_role {
-        role_arn     = "arn:aws:iam::${var.aws_account}:role/${var.aws_role}"
-    }
 
-}
 resource "aws_ami_from_instance" "example" {
      name = var.instance-migration-name
      source_instance_id = var.source_instance_id
