a03d1d59d6f813f65f045e8319b52b40b60a2f08
IBRAHIM <59605442+IBRAHIMCH3@users.noreply.github.com>
2021-10-20 12:25:56 +0530

    Update ecs.tf
    updated

--- ecs.tf
+++ ecs.tf
@@ -299,11 +299,13 @@ resource "aws_ecs_service" "django" {
   task_definition = aws_ecs_task_definition.django.arn
   desired_count   = var.django_tasks_count
   launch_type     = "EC2"
+  /*
   load_balancer {
     target_group_arn = aws_lb_target_group.django.arn
     container_name   = var.django-container_name
     container_port   = var.django-container_port
   }
+  */
 }
 resource "aws_ecs_service" "celery" {
   name            = "${var.env_name}-celery-service"
@@ -318,11 +320,13 @@ resource "aws_ecs_service" "bpm" {
   task_definition = aws_ecs_task_definition.bpm.arn
   desired_count   = var.bpm_tasks_count
   launch_type     = "EC2"
+  /*
   load_balancer {
     target_group_arn = aws_lb_target_group.django.arn
     container_name   = var.bpm-container_name
     container_port   = var.bpm-container_port
   }
+  */
 }
 resource "aws_ecs_service" "frontend" {
   name            = "${var.env_name}-frontend-service"
@@ -330,10 +334,12 @@ resource "aws_ecs_service" "frontend" {
   task_definition = aws_ecs_task_definition.frontend.arn
   desired_count   = var.frontend_tasks_count
   launch_type     = "EC2"
+  /*
   load_balancer {
     target_group_arn = aws_lb_target_group.django.arn
     container_name   = var.frontend-container_name
     container_port   = var.frontend-container_port
   }
+  */
 }
 
